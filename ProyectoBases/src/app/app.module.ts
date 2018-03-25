6466c03610ea5a4b7b51eea2a80764162ef3b82a
andreduard <andreguti333@gmail.com>
2018-03-25 02:19:43 -0600

    Creacion Componentes

--- ProyectoBases/src/app/app.module.ts
+++ ProyectoBases/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegistroComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
