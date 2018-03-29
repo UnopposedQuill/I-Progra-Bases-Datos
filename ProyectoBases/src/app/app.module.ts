fd8d4c3a1f607af93fe07261d82915aeb711e176
andreduard <andreguti333@gmail.com>
2018-03-29 13:28:41 -0600

    Creacion de menu con redireccion

--- ProyectoBases/src/app/app.module.ts
+++ ProyectoBases/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
 import { MenuComponent } from './shared/menu/menu.component';
+import { RouterConfig } from './router.config';
+import { Router, RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -18,7 +20,8 @@ import { MenuComponent } from './shared/menu/menu.component';
     MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(RouterConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
