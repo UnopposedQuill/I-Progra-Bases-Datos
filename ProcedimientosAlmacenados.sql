05d54a07221efa4f5e5d20e1d513e57f4dafb2b9
UnopposedQuill <darklightbeing@gmail.com>
2018-04-04 05:03:08 -0600

    Modificado el batch de procedimientos almacenados de tal forma de que se puedan colocar todos en el mismo query

--- ProcedimientosAlmacenados.sql
+++ ProcedimientosAlmacenados.sql
@@ -1,16 +1,19 @@
 
-use [I Progra]
+use [I_Progra]
+go
+
+create procedure SPeliminarProfesor @nombre nvarchar(50) not null, @email nvarchar(50) not null, @contraseña nvarchar(8) not null
+as begin
+	set nocount on;
+	declare @idProfesorAEliminar as int = 0;
+	set @idProfesorAEliminar = 0;
+	select P.id from Profesor P where P.email = @email;
+	insert into Profesor(nombre, email, contraseña) values(@nombre, @email, @contraseña);
+end
 go
 
 create procedure SPinsertarProfesor @nombre nvarchar(50) not null, @email nvarchar(50) not null, @contraseña nvarchar(8) not null
 as begin
 	set nocount on;
 	return insert into Profesor(nombre, email, contraseña) values(@nombre, @email, @contraseña);
-end
-
-create procedure SPeliminarProfesor @nombre nvarchar(50) not null, @email nvarchar(50) not null, @contraseña nvarchar(8) not null
-as begin
-	set nocount on;
-	declare @idProfesorAEliminar as int =  select P.id from Profesor P where P.email = @email;
-	insert into Profesor(nombre, email, contraseña) values(@nombre, @email, @contraseña);
 end
